ee373a00f81c82e49c78638764a8d22702a6a951
Code12x <87677172+Code12x@users.noreply.github.com>
2022-07-27 17:40:21 -0400

    Bug Fixes
    * SettingsGui didn't move worlds over when a world was deleted using /deleteworld.

--- src/main/java/com/code12/worldtp/worldtpobjects/WorldTPWorldGroup.java
+++ src/main/java/com/code12/worldtp/worldtpobjects/WorldTPWorldGroup.java
@@ -4,7 +4,9 @@ import com.code12.worldtp.files.DataManager;
 import com.code12.worldtp.files.References;
 import lombok.Setter;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.inventory.ItemStack;
 
@@ -48,23 +50,39 @@ public class WorldTPWorldGroup {
         }
 
         //remove the world from "worldGroup" in WorldTP data.yml
-        List<String> menuGroupList = data.getConfig().getStringList("menuGroupList");
-        for (String menuGroup : menuGroupList){
-            if(menuGroup.equals(worldName)){
-                data.getConfig().set("worldGroup." + menuGroup, null);
-            }
-        }
+        data.getConfig().set("worldGroup." + worldName, null);
 
         //remove the world from "menuGroupID" in WorldTP config.yml
         data.getConfig().set("menuGroupID." + worldName, null);
 
-        //remove the player locations for the world group
+        //Check if the deleted world was the lobby world. If so, remove the lobby
+        World lobbyWorld = data.getConfig().getLocation("lobby").getWorld();
+        WorldTPWorld lobbyWorldTPWorld = new WorldTPWorld(lobbyWorld);
+        String lobbyWorldGroup = lobbyWorldTPWorld.getWorldGroup();
+        System.out.println(lobbyWorldGroup);
+        System.out.println(worldName);;
 
+        if(worldName.equals(lobbyWorldGroup)){
+            data.getConfig().set("lobby", null);
+        }
 
         //remove the registered world in the "menuGroupList" List in WorldTP config.yml
-        List<String> worldList = data.getConfig().getStringList("menuGroupList");
-        worldList.remove(worldName);
-        data.getConfig().set("menuGroupList", worldList);
+        List<String> menuGroupList = data.getConfig().getStringList("menuGroupList");
+        menuGroupList.remove(worldName);
+        data.getConfig().set("menuGroupList", menuGroupList);
+
+        //Update the positions of the worlds in menuGroupID.<world name>.position so that no gaps are created in the SelectWorldToEditGui
+        int deletedWorldPosition = data.getConfig().getInt("menuGroupID." + worldName + ".position");
+
+        List<String> oldMenuGroupList = data.getConfig().getStringList("menuGroupList");
+        for(String menuGroup : oldMenuGroupList){
+            int menuGroupPosition = data.getConfig().getInt("menuGroupID." + menuGroup + ".position");
+
+            if(menuGroupPosition > deletedWorldPosition){
+                menuGroupPosition -= 1;
+                data.getConfig().set("menuGroupID." + menuGroup + ".position", menuGroupPosition);
+            }
+        }
 
         data.saveConfig();
 
